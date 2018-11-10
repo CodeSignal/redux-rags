0c295ab56af9c33388bac4b4dd3f702e7a731023
Kyle Saxberg <kyle@codefights.com>
2018-11-10 12:13:53 -0800

    remote throw

--- src/utils/warning.js
+++ src/utils/warning.js
@@ -1,9 +1,6 @@
+// @flow
 export default function warning(message) {
   if (typeof console !== 'undefined' && typeof console.error === 'function') {
     console.error(message);
   }
-
-  try {
-    throw new Error(message);
-  } catch (e) {}
 }
