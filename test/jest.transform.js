3fe8e3d26c0af210239b2b5a11dcf7e298b673d2
Kyle Saxberg <kyle@codefights.com>
2018-12-02 13:45:56 -0800

    okay, need runtime for flow. dev dependency then

--- test/jest.transform.js
+++ test/jest.transform.js
@@ -1,6 +1,7 @@
 module.exports = require('babel-jest').createTransformer({
     presets: ['flow', 'env', 'stage-3'],
     plugins: [
+        'babel-plugin-transform-runtime',
         'syntax-dynamic-import',
         'babel-plugin-dynamic-import-node',
     ]
