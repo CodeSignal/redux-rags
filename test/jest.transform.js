ac9f5d78a7e479040960be130f4b7df3c30f425d
Kyle Saxberg <kyle@codefights.com>
2018-12-02 13:42:04 -0800

    need to remove babel-runtime from test as well

--- test/jest.transform.js
+++ test/jest.transform.js
@@ -1,8 +1,6 @@
 module.exports = require('babel-jest').createTransformer({
     presets: ['flow', 'env', 'stage-3'],
     plugins: [
-        'babel-plugin-transform-runtime',
-        'babel-plugin-transform-class-properties',
         'syntax-dynamic-import',
         'babel-plugin-dynamic-import-node',
     ]
